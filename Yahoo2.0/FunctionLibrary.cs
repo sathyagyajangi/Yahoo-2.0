329acf158b72a21a9e5b7fbfa4709550d8c8be57
Sathyanarayana Gyajangi <Sathyanarayana@sportzinteractive.com>
2019-12-03 18:54:22 +0530

    2

--- Yahoo2.0/FunctionLibrary.cs
+++ Yahoo2.0/FunctionLibrary.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿
+using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using System;
@@ -13,86 +14,143 @@ namespace Yahoo2._0
     class FunctionLibrary
     {
 
-        
 
 
-            public static void clickAction(IWebDriver driver, string LocaterValue, string LocaterType)
+
+        public static void clickAction(IWebDriver driver, string LocaterValue, string LocaterType)
+        {
+            if (LocaterType == "id")
             {
-          
-                if (LocaterType == "xpath")
-                {
-                    driver.FindElement(By.XPath(LocaterValue)).Click();
-                }
-
-
+                driver.FindElement(By.Id(LocaterValue)).Click();
+            }
+            if (LocaterType == "xpath")
+            {
+                driver.FindElement(By.XPath(LocaterValue)).Click();
             }
 
-            public static void TypeAction(IWebDriver driver, string LocaterValue, string LocaterType, string Value)
+
+        }
+
+        public static void TypeAction(IWebDriver driver, string LocaterValue, string LocaterType, string Value)
+        {
+            if (LocaterType == "id")
             {
-                if (LocaterType == "id")
-                    driver.FindElement(By.Id(LocaterValue)).Clear();
+                driver.FindElement(By.Id(LocaterValue)).Clear();
                 driver.FindElement(By.Id(LocaterValue)).SendKeys(Value);
-                if (LocaterType == "xpath")
-                {
-                    driver.FindElement(By.XPath(LocaterValue)).Clear();
-                    driver.FindElement(By.XPath(LocaterValue)).SendKeys(Value);
-                }
-            }
-
-            public static void MouseOver(IWebDriver driver, string LocaterValue)
-
-            {
-                IWebElement element = driver.FindElement(By.XPath(LocaterValue));
-
-                Actions action = new Actions(driver);
-
-                action.MoveToElement(element).Perform();
-
 
             }
-
-
-            public static bool waitForElement(IWebDriver driver, string Locatervalue,Boolean data)
-
+            if (LocaterType == "xpath")
             {
+                driver.FindElement(By.XPath(LocaterValue)).Clear();
+                driver.FindElement(By.XPath(LocaterValue)).SendKeys(Value);
+            }
+        }
 
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromMinutes(1));
+        public static void MouseOver(IWebDriver driver, string LocaterValue)
 
-                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(Locatervalue)));
+        {
+            IWebElement element = driver.FindElement(By.XPath(LocaterValue));
+
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(); ", element);
+
+            Actions action = new Actions(driver);
+
+            action.MoveToElement(element).Perform();
+
+
+        }
+
+
+        public static void waitForElement(IWebDriver driver, string Locatervalue)
+
+        {
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromMinutes(1));
+
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(Locatervalue)));
+
+
+        }
+
+        public static void screenShot(IWebDriver driver)
+        {
+
+            string imgName = DateTime.Now.ToString("dd/MM/yyyy-HH-mm-ss");
+
+
+            Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+
+            ss.SaveAsFile("D:\\ISLproject\\ScreenShots\\" + imgName + ".png");
+
+        }
+
+        public static string ReadDataExcel(int S, int i, int j)
+        {
+            excel.Application xlapp = new excel.Application();
+
+            excel.Workbook xlworkbook = xlapp.Workbooks.Open(@"D:\yahoo2.0\yahooinput.xlsx");
+
+            excel._Worksheet xlworksheet = xlworkbook.Sheets[S];
+
+            excel.Range xlrange = xlworksheet.UsedRange;
+
+            string data = xlrange.Cells[i][j].value2;
+
             return data;
 
-            }
 
-            public static void screenShot(IWebDriver driver)
-            {
+        }
 
-                string imgName = DateTime.Now.ToString("dd/MM/yyyy-HH-mm-ss");
+        public static void contextClick(IWebDriver driver, string LocaterValue)
+        {
+            IWebElement element = driver.FindElement(By.XPath(LocaterValue));
 
+            Actions action = new Actions(driver);
 
-                Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+            action.ContextClick(element).Perform();
 
-                ss.SaveAsFile("D:\\ISLproject\\ScreenShots\\" + imgName + ".png");
+            action.SendKeys(Keys.ArrowRight).Perform();
 
-            }
-
-            public static string ReadDataExcel(int S, int i, int j)
-            {
-                excel.Application xlapp = new excel.Application();
-
-                excel.Workbook xlworkbook = xlapp.Workbooks.Open(@"D:\yahoo2.0\yahooinput.xlsx");
-
-                excel._Worksheet xlworksheet = xlworkbook.Sheets[S];
-
-                excel.Range xlrange = xlworksheet.UsedRange;
-
-                string data = xlrange.Cells[i][j].value2;
-
-                return data;
+            action.SendKeys(Keys.Enter).Perform();
 
 
-            }
+
+        }
+
+        public static void CtrlClick(IWebDriver driver, string Locatervalue)
+        {
+            IWebElement element = driver.FindElement(By.XPath(Locatervalue));
+
+            Actions action = new Actions(driver);
+
+            action.MoveToElement(element);
+
+            action.KeyDown(Keys.Control).Perform();
+
+            action.Click().Perform();
+
+        }
+
+        public static string ElementText(IWebDriver driver, string locaterValue)
+        {
+
+            string text = driver.FindElement(By.XPath(locaterValue)).Text;
 
 
-        
+            return text;
+
+        }
+
+        public static void ctrlW(IWebDriver driver)
+
+        {
+            Actions action = new Actions(driver);
+
+            action.KeyDown(Keys.Control).SendKeys("W").Perform();
+
+            
+
+        }
+
     }
 }
--- Yahoo2.0/UnitTest1.cs
+++ Yahoo2.0/UnitTest1.cs
@@ -32,7 +32,7 @@ namespace Yahoo2._0
 
             string handle = driver.CurrentWindowHandle;
 
-            for(int i=2;i<=11;i++)
+            for(int i=2;i<=8;i++)
             {
 
                 FunctionLibrary.clickAction(driver, "/html/body/div[1]/header/div/section/div/div[3]/div/nav/ul/li[" + i + "]/a", "xpath");
@@ -48,17 +48,16 @@ namespace Yahoo2._0
                 if(title.Contains("Series"))
                 {
 
-                    Yahoo yahoo = new Yahoo(driver);
-                    yahoo.GetUrl();
+                    FunctionLibrary.clickAction(driver, "/html/body/div[1]/div/myapp/section[2]/div/div/div/div[1]/div/section/div/div[3]/div[2]/a[1]", "xpath");
+                 
 
+                
 
-                    driver.FindElement(By.CssSelector(".si-dv-row")).Click();
-
-                    for (int j=2;j<=10;j++)
+                 for (int j=2;j<=10;j++)
                     {
 
 
-                      FunctionLibrary.clickAction(driver, "/html/body/div[1]/div/myapp/section[1]/div/div/div/div/section/div/div/div[2]/a[" + j + "]", "xpath");
+                      FunctionLibrary.clickAction(driver, "/html/body/div[1]/div/myapp/section[1]/div/div/div/div/section/div/div/div[2]/div/a[" + j + "]", "xpath");
 
                     
 
@@ -66,7 +65,7 @@ namespace Yahoo2._0
 
                 }
 
-                driver.Navigate().Back();
+               
             }
 
 
@@ -111,7 +110,120 @@ namespace Yahoo2._0
 
             }
 
+            for (int j = 1; j <= 2; j++)
+            {
+
+                FunctionLibrary.clickAction(driver, "//*[@id='486f760d-cf05-45bb-80db-7ebbafb323df']/div/div/div[1]/div[4]/span[" + j + "]", "xpath");
+
+
+
+
+
+
+                for (int a = 2; a <= 3; a++)
+
+                {
+                    FunctionLibrary.MouseOver(driver, "//*[@id='486f760d-cf05-45bb-80db-7ebbafb323df']/div/div/div[1]/div[1]/div/article[" + a +"]/div/div[2]/div[3]/div/div[2]/span");
+
+                    for (int b = 1; b <= 3; b++)
+                    {
+
+                        FunctionLibrary.CtrlClick(driver, "//*[@id='486f760d-cf05-45bb-80db-7ebbafb323df']/div/div/div[1]/div[1]/div/article[" + a +"]/div/div[2]/div[3]/div/div[2]/ul/li[" + b +"]/a");
+
+
+                    }
+
+
+
+
+                }
+
+
+            }
+
+
+            string tiltle = driver.FindElement(By.XPath("/html/body/div[1]/div/myapp/section[2]/div/div/div/div[3]/section[1]/div/div/div[1]/div/h3")).Text;
+
+            if (tiltle.Contains("Trending"))
+            {
+
+                for (int c = 1; c <= 8; c++)
+                {
+                    FunctionLibrary.clickAction(driver, "//*[@id='26c2875c-302c-42c1-b188-2cd13a01fbcf']/div/div/div[2]/div/article[" + c + "]/div/div/a/h2", "xpath");
+
+                    Thread.Sleep(1000);
+                }
+
+
+            }
+
+         
+            for(int d=1;d<=3;d++)
+            {
+
+                FunctionLibrary.CtrlClick(driver, "/html/body/div[1]/div/myapp/section[2]/div/div/div/div[2]/section/div/div/div[2]/div/article[" + d + "]/div/div[2]/a/h3");
+
+                FunctionLibrary.MouseOver(driver, "/html/body/div[1]/div/myapp/section[2]/div/div/div/div[2]/section/div/div/div[2]/div/article[" + d + "]/div/div[2]/div[2]/div/div[2]/span");
+
+                for (int f = 1; f <= 3; f++)
+                {
+
+                    FunctionLibrary.CtrlClick(driver, "/html/body/div[1]/div/myapp/section[2]/div/div/div/div[2]/section/div/div/div[2]/div/article[" + d + "]/div/div[2]/div[2]/div/div[2]/ul/li[" + f + "]/a");
+
+
+                }
+            }
+
+
+
+            for(int e=1;e<=4;e++)
+            {
+
+                FunctionLibrary.MouseOver(driver, "/html/body/div[1]/div/myapp/section[7]/div/div/div/div/section/section[" + e + "]/div/div/div[1]/div/h3");
+
+         string str= FunctionLibrary.ElementText(driver, "/html/body/div[1]/div/myapp/section[7]/div/div/div/div/section/section[" + e + "]/div/div/div[1]/div/h3");
+
+                Console.WriteLine(str);
+
+
+
+            }
+           
+
+
         }
+
+
+
+
+
+        [TestMethod]
+
+        public void temp()
+        {
+
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Url = "https://beta-cricket-yahoo.sportz.io/";
+
+            driver.Manage().Window.Maximize();
+
+
+
+            FunctionLibrary.CtrlClick(driver, "/html/body/div[1]/div/myapp/section[2]/div/div/div/div[3]/section[1]/div/div/div[2]/div/article[1]/div/div/a");
+
+            Thread.Sleep(5000);
+
+
+           
+
+
+
+
+        }
+
+
+
 
 
     }
